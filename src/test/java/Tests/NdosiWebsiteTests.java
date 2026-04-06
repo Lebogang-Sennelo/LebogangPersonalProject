09aed0b0ec22eca9c506514018880396d40d068c
Lebogang <lebosennelo@gmail.com>
2026-04-06 20:42:02 +0200

    Added Base class and Tests class.Added code on home page

---
+++ src/test/java/Tests/NdosiWebsiteTests.java
@@ -0,0 +1,12 @@
+package Tests;
+
+import org.testng.annotations.Test;
+
+@Test
+public class NdosiWebsiteTests extends Base {
+
+  public void clickLoginButton(){
+   homePage.clickLoginButton();
+  }
+
+}
